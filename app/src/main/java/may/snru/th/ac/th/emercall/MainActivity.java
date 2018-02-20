344618abeeea6738e6a11008625078454e523b6f
May <Suphakon.wo59@snru.ac.th>
2018-02-20 12:01:16 +0700

    Add Fragment Success

--- app/src/main/java/may/snru/th/ac/th/emercall/MainActivity.java
+++ app/src/main/java/may/snru/th/ac/th/emercall/MainActivity.java
@@ -2,6 +2,9 @@ package may.snru.th.ac.th.emercall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+
+import may.snru.th.ac.th.emercall.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+ //       Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+
+
+    } // Main Method
+
+} // Main Class
